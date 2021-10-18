ff3e661bbda79c9c9c21f49080835f8f81cb4ece
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-10-18 17:07:23 +0700

    ex2

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -7,62 +7,65 @@ Ex:
 | Output: 8 3 5 6 4 2 7 9 2          |
 |____________________________________|
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
 
-#include<stdio.h>
+void Ex5(int arr[], int n){
+	//Your codes here
+    int odd[n];
+    int even[n];
+    int o_count = 0;
+    int e_count = 0;
 
-void Enter(int a[], int & n){
-int main();{
-    int a[100];
-    int n;
-    Enter(a,n);
-    SortUp(a,n);
-    return 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i]%2 == 0){
+            even[e_count] = arr[i];
+            e_count +=1;
+        }else{
+            odd[o_count] = arr[i];
+            o_count +=1;
+        }
+    }
 
-    do{
-        printf("Enter n: ");
-        scanf("%d",&n);
-    }
-    while(n<2||n>99);
-     
-    for(int i=0; i<n; i++){
-        printf("a[%d]: ",i);
-        scanf("%d",&a[i]);
-    }
-}
- 
-void xuat(int a[], int n){
-    for(int i=0;i<n;i++){
-        printf("%5d",a[i]);
-    }
-}
- 
-//Ham sap xep thu nhat su dung bien trung gian
-void SortUp(int a[], int n){
-    int Mid;
-    for(int i=0;i<n;i++)
-        for(int j=0;j<=i;j++){
-            if(a[j]<a[i]){
-                Mid=a[i];
-                a[i]=a[j];
-                a[j]=Mid;
+    int current, next, temp;
+    for(int j = 0; j < n; j ++){
+        //sort odd is asc
+        for(int i = 0; i < o_count - 1; i++){
+            current = odd[i];
+            next = odd[i + 1];
+            if(current > next){
+                temp = current;
+                odd[i] = next;
+                odd[i + 1] = temp;
             }
         }
-    printf("\nMang sau khi sap xep:\n");
-    xuat(a,n);
-}
-//Ham sap xep thu 2 khong dung bien trung gian
-void SortUp2(int a[], int n){
-    for(int i=0;i<n;i++)
-        for(int j=0;j<=i;j++){
-            if(a[j]<a[i]){
-                a[i]=a[i]+a[j];
-                a[j]=a[i]-a[j];
-                a[i]=a[i]-a[j];
+
+        //sort even is desc
+        for(int i = 0; i < e_count - 1; i++){
+            current = even[i];
+            next = even[i + 1];
+            if(current < next){
+                temp = current;
+                even[i] = next;
+                even[i + 1] = temp;
             }
         }
-         
-    printf("\nMang sau khi sap xep giam:\n");
-    xuat(a,n);  
+    }
+
+    for(int i = 0; i < o_count; i++){
+        if(i == 0){
+            printf("%d", odd[i]);
+        }else{
+            printf(" %d", odd[i]);
+        }
+    }
+    
+    for(int i = 0; i < e_count; i++){
+        printf(" %d", even[i]);
+    }
+}
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
@@ -70,10 +73,13 @@ int main(int argc, char *argv[]) {
 	int testcase[argc],i;
 	for(i=0; i<argc;i++){
 		testcase[i] = atoi(argv[i+1]);
+		// printf("%d ", testcase[i]);
 	}
 	
-	// Ex2(testcase, argc);
-	
+	Ex5(testcase, argc);    
+
+    printf("\n");
+
 	return 0;
-	}
 }
+	
