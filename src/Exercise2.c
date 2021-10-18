829f371c403b427f9145971394b52455422f4d00
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-10-18 16:15:38 +0700

    edit 4

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -8,15 +8,61 @@ Ex:
 |____________________________________|
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include<stdio.h>
 
-void Ex2(int arr[], int n){
-	//Your codes here
-	
+void Enter(int a[], int & n){
+int main();{
+    int a[100];
+    int n;
+    Enter(a,n);
+    SortUp(a,n);
+    return 0;
 
+    do{
+        printf("Enter n: ");
+        scanf("%d",&n);
+    }
+    while(n<2||n>99);
+     
+    for(int i=0; i<n; i++){
+        printf("a[%d]: ",i);
+        scanf("%d",&a[i]);
+    }
 }
+ 
+void xuat(int a[], int n){
+    for(int i=0;i<n;i++){
+        printf("%5d",a[i]);
+    }
+}
+ 
+//Ham sap xep thu nhat su dung bien trung gian
+void SortUp(int a[], int n){
+    int Mid;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<=i;j++){
+            if(a[j]<a[i]){
+                Mid=a[i];
+                a[i]=a[j];
+                a[j]=Mid;
+            }
+        }
+    printf("\nMang sau khi sap xep:\n");
+    xuat(a,n);
+}
+//Ham sap xep thu 2 khong dung bien trung gian
+void SortUp2(int a[], int n){
+    for(int i=0;i<n;i++)
+        for(int j=0;j<=i;j++){
+            if(a[j]<a[i]){
+                a[i]=a[i]+a[j];
+                a[j]=a[i]-a[j];
+                a[i]=a[i]-a[j];
+            }
+        }
+         
+    printf("\nMang sau khi sap xep giam:\n");
+    xuat(a,n);  
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
@@ -26,7 +72,8 @@ int main(int argc, char *argv[]) {
 		testcase[i] = atoi(argv[i+1]);
 	}
 	
-	Ex2(testcase, argc);
+	// Ex2(testcase, argc);
 	
 	return 0;
+	}
 }
